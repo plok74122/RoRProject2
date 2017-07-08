29c12db818f6c83df4ef16775fa99025dab8cc6c
plok74122 <plok74122@gmail.com>
2017-07-09 00:04:52 +0800

    review

--- app/controllers/shorturls_controller.rb
+++ app/controllers/shorturls_controller.rb
@@ -1,30 +1,27 @@
 class ShorturlsController < ApplicationController
+  before_action :set_short_urls, only: [:index, :create]
 
   def index
     # never all
-    @shorturls = Shorturl.page(params[:page])
+    @shorturl = Shorturl.new
   end
 
   def create
-    url_hash = SecureRandom.hex(6)
-    shorturl_params = params.require(:shorturl).permit(:url)
-    @shorturl = Shorturl.new(shorturl_params)
-    @shorturl.short_url = url_hash
+    @shorturl = Shorturl.new(url: params[:shorturl][:url])
     if @shorturl.save
       flash[:notice] = "新增成功"
       redirect_to shorturls_path
     else
-      flash[:alert] = "[!] 請輸入url"
-      redirect_to shorturls_path
+      render :index
     end
   end
 
   def redirect
     if params.has_key?(:q)
-      url = Shorturl.where(:short_url => params[:q])
-      if url.exists?
-        url.update(:convert_num => url[0].convert_num + 1)
-        redirect_to url[0].url
+      url = Shorturl.find_by(:short_url => params[:q])
+      if url
+        url.update(:convert_num => url.convert_num + 1)
+        redirect_to url.url
       else
         flash[:alert] = "參數q錯誤"
         redirect_to root_path
@@ -33,6 +30,12 @@ class ShorturlsController < ApplicationController
       flash[:alert] = "需要有參數q"
       redirect_to root_path
     end
+  end
+
+  private
+
+  def set_short_urls
+    @shorturls = Shorturl.page(params[:page])
   end
 
 end
--- app/models/shorturl.rb
+++ app/models/shorturl.rb
@@ -1,3 +1,15 @@
 class Shorturl < ApplicationRecord
   validates_presence_of :url
+  validates_uniqueness_of :url
+  before_create :generate_unique_string
+
+  private
+
+  def generate_unique_string
+    unique_string = SecureRandom.hex(6)
+    while Shorturl.find_by_short_url( unique_string ).present? do
+      unique_string = SecureRandom.hex(6)
+    end
+    self.short_url = unique_string
+  end
 end
